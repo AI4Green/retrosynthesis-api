6bc6e81533ffee14ed86741d1e07e343886904c5
Joe Davies <Joseph.Davies@nottingham.ac.uk>
2024-08-07 16:21:55 +0100

    upgraded to aizynthfinder 4.0

--- sources/retrosynthesis/aizynthfinder/context/stock/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Sub-package containing stock routines
-"""
-from ....aizynthfinder.context.stock.queries import MongoDbInchiKeyQuery, StockQueryMixin
-from ....aizynthfinder.context.stock.stock import Stock
-from ....aizynthfinder.utils.exceptions import StockException
