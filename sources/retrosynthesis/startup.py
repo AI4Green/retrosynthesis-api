321137a1d54407b74cea77ce62ef826219acdceb
Joe Davies <Joseph.Davies@nottingham.ac.uk>
2024-08-14 15:26:45 +0100

    molbloom added

--- sources/retrosynthesis/startup.py
+++ sources/retrosynthesis/startup.py
@@ -1,66 +1,55 @@
 import os
 from flask import request
 from sources.retrosynthesis.classes import AiZynthArgs
-# from sources.retrosynthesis.aizynthfinder import aizynthfinder
-# from sources.retrosynthesis.aizynthfinder.interfaces import aizynthcli
 from aizynthfinder import aizynthfinder
 from aizynthfinder.interfaces import aizynthcli
 
 
 def make_config():
-   BASEDIR = os.path.dirname(__file__)
+    BASEDIR = os.path.dirname(__file__)
 
-   stock_request = request.args.get('stock')
+    stock_request = request.args.get('stock')
 
-   stock_file_dict = {'emolecules': 'emolecules.hdf5',
+    stock_file_dict = {'emolecules': 'emolecules.hdf5',
                        'zinc': 'zinc_stock_17_04_20.hdf5'}
 
-   chosen_stock_file = stock_file_dict.get(stock_request, 'bloom')
+    chosen_stock_file = stock_file_dict.get(stock_request, 'bloom')
 
-   AIZYNTH = {
-      'expansion': {
-         'full': {
-            'type': 'template-based',
-            'model': os.path.join(BASEDIR, 'config_files', 'uspto_model.onnx'),
-            'template': os.path.join(BASEDIR, 'config_files', 'uspto_templates.csv.gz')
-         }
-      },
+    AIZYNTH = {
+        'expansion': {
+            'full': {
+                'type': 'template-based',
+                'model': os.path.join(BASEDIR, 'config_files', 'uspto_model.onnx'),
+                'template': os.path.join(BASEDIR, 'config_files', 'uspto_templates.csv.gz')
+            }
+        },
 
+        'stock': {'bloom': os.path.join(BASEDIR, 'config_files', 'zinc_and_emol_inchi_key.bloom')}
+        ,
+        'config_file': os.path.join(BASEDIR, 'config_files', 'aizynthfinder_config.yml'),
+        'properties': {
+            'max_transforms': 10,
+            'time_limit': 3600,
+            'iteration_limit': 500,
+        }
+    }
 
-         # 'files': {
-         #    'uspto': [os.path.join(BASEDIR, 'config_files', 'uspto_model.onnx'),
-         #              os.path.join(BASEDIR, 'config_files', 'uspto_templates.csv.gz')]}},
-      'stock': {'bloom': os.path.join(BASEDIR, 'config_files', 'emol.bloom')}
-         # 'files': {
-         #    # 'zinc': os.path.join(BASEDIR, 'config_files', 'zinc_stock_17_04_20.hdf5'),
-         #    # 'stock': os.path.join(BASEDIR, 'config_files', chosen_stock_file)
-         #
-         # }
-      ,
-      'config_file': os.path.join(BASEDIR, 'config_files', 'aizynthfinder_config.yml'),
-      'properties': {
-         'max_transforms': 10,
-         'time_limit': 3600,
-         'iteration_limit': 500,
-      }
-   }
-
-   print("1")
-   print(AIZYNTH)
-   aizynth_config_dic = AIZYNTH
-   print(2)
-   # initiate object containing all required arguments
-   args = AiZynthArgs("placeholder",  aizynth_config_dic['expansion'],
-                      aizynth_config_dic['stock'])
-   print(3)
-   # AiZynthFinder object contains results data
-   finder = aizynthfinder.AiZynthFinder(configdict=aizynth_config_dic)
-   print(4)
-   # set up stocks, policies, then start single smiles process
-   aizynthcli._select_stocks(finder, args)
-   print(5)
-   finder.expansion_policy.select(args.policy or finder.expansion_policy.items[0])
-   print(6)
-   finder.filter_policy.select(args.filter)
-   print(7)
-   return finder
+    print("1")
+    print(AIZYNTH)
+    aizynth_config_dic = AIZYNTH
+    print(2)
+    # initiate object containing all required arguments
+    args = AiZynthArgs("placeholder",  aizynth_config_dic['expansion'],
+                       aizynth_config_dic['stock'])
+    print(3)
+    # AiZynthFinder object contains results data
+    finder = aizynthfinder.AiZynthFinder(configdict=aizynth_config_dic)
+    print(4)
+    # set up stocks, policies, then start single smiles process
+    aizynthcli._select_stocks(finder, args)
+    print(5)
+    finder.expansion_policy.select(args.policy or finder.expansion_policy.items[0])
+    print(6)
+    finder.filter_policy.select(args.filter)
+    print(7)
+    return finder
