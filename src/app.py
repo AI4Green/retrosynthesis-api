23cd9e3cecc8bd1aa8119e6d1e6e3791056c41c5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-10-10 14:13:35 +0100

    start rs process with the app

--- src/app.py
+++ src/app.py
@@ -1,5 +1,14 @@
+from multiprocessing import Process
+
 from sources import app
+from sources.retrosynthesis.worker import results, queue, worker
 
 
 if __name__ == '__main__':
+	# Set up the worker process in the background to run
+	# the retrosynthesis without blocking the API
+	p = Process(target=worker, args=(queue, results), daemon=True)
+	p.start()
+
+	# Run the API
 	app.run(debug=False, host='0.0.0.0', port=8000)
